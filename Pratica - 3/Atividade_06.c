0abc23821d01cfa1d54c4e21edce8dd2affd319e
Adalberto <adalbertob.vieira@gmail.com>
2018-09-04 16:06:34 -0300

    atividade

--- Pratica - 3/Atividade_06.c
+++ Pratica - 3/Atividade_06.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <Math.h>
+unsigned long long fast_pow_2(int expoente){
+    return 2 << expoente-1;
+}
+int ddd(unsigned int number){
+    int cont=0,ddd;
+    unsigned int aux=number;
+    do{
+        aux = aux / 10;
+        cont+=1;
+    }while(aux != 0);
+    ddd=((number/pow(10,cont-1))*10+(number/pow(10,cont-2)))/2;
+    return ddd;
+}
+void main(){
+    printf("%d", ddd(3634095858));
+}
 
