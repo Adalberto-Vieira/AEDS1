1e5a865395c9343999fe29b6ef3e40281f503ade
Adalberto Vieira <adalbertob.vieira@gmail.com>
2018-09-05 17:16:23 -0300

    Q2

--- Pratica - 4/pratica4ex2.c
+++ Pratica - 4/pratica4ex2.c
@@ -1,1 +1,6 @@
+#include "pratic4ex2.h"
+#include <Math.h>
 
+float areaCirculo(float raio){
+    return 2*M_PI&raio;
+}
--- Pratica - 4/pratica4ex2.h
+++ Pratica - 4/pratica4ex2.h
@@ -1,17 +1,4 @@
-#ifndef PRATICA4_H
-#define PRATICA4_H
-    float paraMetrosPorSegundo(float v);
+#ifndef PRATICA4ex2_H
+#define PRATICA4ex2_H
     float areaCirculo(float raio);
-    int ehPar(int n);
-    int ehDivisivelPor3ou5(int n);
-    float pesoIdeal(float h, char sexo);
-    int somaImpares(int N);
-    double fatorial(int N);
-    int somaNumerosDiv3ou5(int N);
-    float calculaMedia(int x, int y, int z, int operacao);
-    int numeroDivisores(int N);
-    int enesimoFibonacci(int N);
-    int mdc(unsigned int x, unsigned int y);
-    int mmc(unsigned int x, unsigned int y);
-
 #endif
