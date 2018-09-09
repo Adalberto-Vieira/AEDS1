bd260139a5b7f736dcb222a57039b49764a3bc23
Adalberto <adalbertob.vieira@gmail.com>
2018-09-08 22:22:02 -0300

    lots of random stuf

--- Pratica - 4/pratica4.c
+++ Pratica - 4/pratica4.c
@@ -2,7 +2,7 @@
 #include <Math.h>
 
 float areaCirculo(float raio){
-    return 2*M_PI*raio;
+    return M_PI*raio*raio;
 }
 
 float paraMetrosPorSegundo(float v){
---
+++ Pratica - 4/testePratica4.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <locale.h>
+#include "pratica4.c"
+
+void main(){
+    /* Localiza o codigo para imprimir caracteres especiais. */
+    setlocale(LC_ALL, "");
+    int op=1, numero;
+    float diametroBase,altura, largura, comprimento;
+    float velocidade,raio;
+    while(op!=0){
+        //menu de caracteres ASCII
+        printf("|--------------------|-------------------|\n");
+        printf("| 1 - Exercicio 1    | 2 - Exercicio 2   |\n");
+        printf("|--------------------|-------------------|\n");
+        printf("| 3 - Exercicio 3    | 4 - Exercicio 4   |\n");
+        printf("|--------------------|-------------------|\n");
+        printf("| 5 - Exercicio 5    | 6 - Exercicio 6   |\n");
+        printf("|--------------------|-------------------|\n");
+        printf("| 7 - Exercicio 7    | 8 - Exercicio 8   |\n");
+        printf("|--------------------|-------------------|\n");
+        printf("| 9 - Exercicio 9    | 10 - Exercicio 10 |\n");
+        printf("|--------------------|-------------------|\n");
+        printf("| 11 - Exercicio 11  | 12 - Exercicio 12 |\n");
+        printf("|--------------------|-------------------|\n");
+        printf("| 13 - Exercicio 13  | 14 - Exercicio 14 |\n");
+        printf("|--------------------|-------------------|\n");
+        printf("| 15 - Exercicio 15  | 16 - Exercicio 16 |\n");
+        printf("|--------------------|-------------------|\n");
+        printf("| 17 - Todas         | 0 - SAIR           |\n");
+        printf("|--------------------|-------------------|\n");
+        printf("Opção desejada: ");
+        scanf("%d",&op);
+        switch(op){
+            case 1:
+
+                printf("\nDigite a velocidade em Km/h: ");
+                scanf("%f",&velocidade);
+                printf("\nA velocidade equivale a %f m/s\n",paraMetrosPorSegundo(velocidade));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 2:
+                printf("\nDigite o raio: ");
+                scanf("%f", &raio);
+                printf("\nA area é: %f\n", areaCirculo(raio));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 3:
+                printf("\nDigite o número ");
+                scanf("%d", &numero);
+                printf("\n%d\n",ehPar(numero));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 4:
+                printf("\nDigite o sexo: ");
+                scanf("%c", &sexo);
+                printf("\n%d\n",ehDivisivelPor3ou5(numero));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 5:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 6:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 7:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 8:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 9:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 10:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 11:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 12:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 13:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 14:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 15:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 16:
+                printf("\nDigite a altura em cm: ");
+                scanf("%f", &altura);
+                printf("\nDigite o diametro da base em cm: ");
+                scanf("%f",&diametroBase);
+                printf("\nO volume do solido é %.2f cm cubicos\n",(altura*diametroBase));
+                system("PAUSE");
+                system("cls");
+            break;
+            case 0:
+                printf("Até logo");
+            break;
+            default:
+                printf("Valor não reconhecido como um comando valido\n");
+                system("PAUSE");
+                system("cls");
+            break;
+        }
+    }
+}
