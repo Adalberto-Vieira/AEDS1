5ac309d355a96610790fb1975b96f7515d63356d
Adalberto <adalbertob.vieira@gmail.com>
2018-09-17 21:45:25 -0300

    random stuff

---
+++ Trabalho final/estatistica.h
@@ -0,0 +1,10 @@
+#ifndef ESTATISTICA_H
+#define ESTATISTICA_H
+#include <Math.h>
+#include <stdio.h>
+#include <stdio.h>
+
+double cauchy(double x);
+double gumbel(double x, double y, double b);
+double laplace(double x, double y, double b);
+#endif
